2c2e85f6a6ffd370c5dd135e5d5ac3fd5a792a52
GheorgheAnca <142331292+GheorgheAnca@users.noreply.github.com>
2024-03-13 14:02:02 +0100

    task-09-completed

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const buttonElement = document.querySelector('.change-color');
+const spanElement = document.querySelector('.color');
+
+buttonElement.addEventListener('click', function() {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  spanElement.textContent = newColor;
+});
