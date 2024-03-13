a20d486d35c8a24729938ab9ada18d49fa59f4db
GheorgheAnca <142331292+GheorgheAnca@users.noreply.github.com>
2024-03-13 13:59:45 +0100

    task-08-completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
 
+const formElement = document.querySelector('.login-form');
+
+formElement.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const formData = new FormData(formElement);
+  const formObject = {};
+
+  for (const [name, value] of formData.entries()) {
+    if (value === '') {
+      alert('Toate câmpurile trebuie completate.');
+      return;
+    }
+    formObject[name] = value;
+  }
+
+  console.log(formObject);
+  formElement.reset();
+});
